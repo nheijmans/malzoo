51d3b6d1a327dbc2370e4c6714acb96ef58e5606
Niels Heijmans <niels_heijmans@icloud.com>
2019-01-30 15:39:00 +0100

    updated apt-get command in bootstrap.sh with noninteractive variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,13 @@ echo "[*] Starting installation of Malzoo & Co. (requirements)..."
 sleep 2
 cd $HOME
 sudo apt-get update
-sudo apt-get upgrade -yq
-sudo apt-get install -yq vim build-essential python-dev libtool bison autoconf python-magic tmux ssdeep git unzip zip python-pip python-bottle python-requests libldap-dev libsasl2-dev libldap2-dev libssl-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq vim build-essential python-dev libtool bison autoconf python-magic tmux ssdeep git unzip zip python-pip python-bottle python-requests libldap-dev libsasl2-dev libldap2-dev libssl-dev
 #Mongo
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq mongodb-org
 export LC_ALL=C
 sudo service mongod start
 sleep 2
