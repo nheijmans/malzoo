1730abf0516b3e71fff529eb46646bfe7f0469a4
nheijmans <niels_heijmans@hotmail.com>
2020-12-12 14:11:12 +0100

    replaced print with print() in virustotal module

--- malzoo/modules/virustotal.py
+++ malzoo/modules/virustotal.py
@@ -21,27 +21,27 @@ class VirusTotal(CustomModule):
 
     def get_result(self,a):
         for k,v in a.items():
-            print k,':',v
+            print(k,':',v)
 
         payload  = {'resource':a['md5'], 'apikey':self.api_key}
         response = requests.get(self.base_url, params=payload, timeout=5.0)
         jdata    = json.loads(response.text)
-        print "virustotal",jdata
+        print("virustotal",jdata)
     
         if jdata['response_code'] == 1:
-            print "got results"
+            print("got results")
         else:
-            print "not yet in VT"
+            print("not yet in VT")
         
         return
 
     def run(self):
         try:
-            print self.name, 'is running'
+            print(self.name, 'is running')
             self.get_result(self.pkg)
             self.share_data("text")
         except Exception as e:
-            print "vt",e
+            print("vt",e)
             self.log('{0} - {1} - {2}'.format('custom module',self.name,e))
         finally:
             return
