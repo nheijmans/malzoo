a85fbdf165e0b3ae7a860b7f3c940da9a3ae7d11
nheijmans <niels_heijmans@hotmail.com>
2021-11-04 13:26:54 +0100

    removing try/except to see whats causing the decode error

--- malzoo/core/workers/peworker.py
+++ malzoo/core/workers/peworker.py
@@ -18,38 +18,38 @@ from malzoo.core.tools.pe           import PeInfo
 class PEWorker(Worker):
     def process(self, sample):
         """ This function will process the PE files """
-        try:
+        #try:
             # Create objects from the classes
-            hasher       = Hasher(sample['filename'])
-            general_info = GeneralInformation(sample['filename'])
-            pe_info      = PeInfo(sample['filename'], 'data/userdb.txt')
-            sigs_yara    = Signatures()
+        hasher       = Hasher(sample['filename'])
+        general_info = GeneralInformation(sample['filename'])
+        pe_info      = PeInfo(sample['filename'], 'data/userdb.txt')
+        sigs_yara    = Signatures()
     
-            # Creating a dictionary with sample information
-            sample_info = { 
-            'filename'          : general_info.get_filename(),
-            'filetype'          : general_info.get_filetype(),
-            'filesize'          : str(general_info.get_filesize()),
-            'md5'               : hasher.get_md5(),
-            'sha1'              : hasher.get_sha1(),
-            'pehash'            : hasher.get_pehash(),
-            'imphash'           : hasher.get_imphash(),
-            'fuzzy'             : hasher.get_fuzzy(),
-            'yara_results'      : sigs_yara.scan(sample['filename']),
-            'pe_compiletime'    : pe_info.get_compiletime(),
-            'pe_dll'            : pe_info.get_dll(),
-            'pe_packer'         : pe_info.packer_detect(),
-            'pe_language'       : pe_info.get_language(),
-            'original_filename' : pe_info.get_org_filename(),
-            'submit_date'       : int(time()),
-            'sample_type'       : 'exe',
-            'id_tag'            : sample['tag']
-            }
+        # Creating a dictionary with sample information
+        sample_info = { 
+        'filename'          : general_info.get_filename(),
+        'filetype'          : general_info.get_filetype(),
+        'filesize'          : str(general_info.get_filesize()),
+        'md5'               : hasher.get_md5(),
+        'sha1'              : hasher.get_sha1(),
+        'pehash'            : hasher.get_pehash(),
+        'imphash'           : hasher.get_imphash(),
+        'fuzzy'             : hasher.get_fuzzy(),
+        'yara_results'      : sigs_yara.scan(sample['filename']),
+        'pe_compiletime'    : pe_info.get_compiletime(),
+        'pe_dll'            : pe_info.get_dll(),
+        'pe_packer'         : pe_info.packer_detect(),
+        'pe_language'       : pe_info.get_language(),
+        'original_filename' : pe_info.get_org_filename(),
+        'submit_date'       : int(time()),
+        'sample_type'       : 'exe',
+        'id_tag'            : sample['tag']
+        }
     
-            self.share_data(sample_info)
-            self.store_sample(sample['filename'])
-        except Exception as e:
-            self.log('{0} - {1} - {2} '.format('peworker',sample,e))
-            traceback.print_exc()
-        finally:
-            return
+        self.share_data(sample_info)
+        self.store_sample(sample['filename'])
+        #except Exception as e:
+        #    self.log('{0} - {1} - {2} '.format('peworker',sample,e))
+        #    traceback.print_exc()
+        #finally:
+        return
